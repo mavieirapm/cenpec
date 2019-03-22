6899511884307722ee34ab2bc0284e169d2fb899
mavieirapm <30505889+mavieirapm@users.noreply.github.com>
2019-03-22 15:22:18 -0300

    Add files via upload

--- code/js/index.js
+++ code/js/index.js
@@ -29,15 +29,3 @@ function highlight_map_states() {
         })
     }
 };
-
-
-function flip(event) {
-    var element = event.currentTarget;
-    if (element.className === "card") {
-        if (element.style.transform == "rotateY(180deg)") {
-            element.style.transform = "rotateY(0deg)";
-        } else {
-            element.style.transform = "rotateY(180deg)";
-        }
-    }
-};
